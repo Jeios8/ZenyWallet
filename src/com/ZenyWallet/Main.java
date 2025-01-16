e18e2c8860153781ea125eb45a13f9f963ed12e1
Jeios8 <rjohngdc@gmail.com>
2025-01-16 20:04:01 +0800

    - changed database system from MySQL to SQLite

--- src/com/ZenyWallet/Main.java
+++ src/com/ZenyWallet/Main.java
@@ -1,18 +1,13 @@
 package com.ZenyWallet;
 
-import com.ZenyWallet.model.User;
 import com.ZenyWallet.model.UserRepository;
 import com.ZenyWallet.view.WalletView;
 import com.ZenyWallet.viewmodel.WalletViewModel;
 
-import java.sql.SQLException;
-
 public class Main {
     public static void main(String[] args) {
-        String db_url = "jdbc:mysql://127.0.0.1:3306/user_schema";
-        String db_username = "root";
-        String db_password = "2243519278";
-        UserRepository userRepository = new UserRepository(db_url, db_username, db_password);
+        String db_url = "jdbc:sqlite:src/resources/users.db";
+        UserRepository userRepository = new UserRepository(db_url);
         WalletView walletView = new WalletView();
         WalletViewModel walletViewModel = new WalletViewModel(walletView, userRepository);
 
--- src/com/ZenyWallet/viewmodel/WalletViewModel.java
+++ src/com/ZenyWallet/viewmodel/WalletViewModel.java
@@ -38,11 +38,24 @@ public class WalletViewModel {
         String pin;
 
         for (int i = 0; i < 3; i++) {
+            walletView.displayMessage("\nEnter 0 to cancel login.");
             userID = walletView.getUserInput("Enter UserID: ");
-            if (userID != null && userID.matches("\\d{6}") && userRepository.authenticateUserID(userID)) {
+            // Check if the user entered "0" to cancel the registration
+            if (userID.equals("0")) {
+                walletView.displayMessage("Login canceled.");
+                return;  // Exit the login process
+            }
+
+            if (userID.matches("\\d{6}") && userRepository.authenticateUserID(userID)) {
                 for (int j = 0; j < 3; j++) {
+                    walletView.displayMessage("\nEnter 0 to cancel login.");
                     pin = walletView.getUserInput("Enter PIN: ");
-                    if (pin != null && pin.matches("\\d{4}") && userRepository.authenticatePIN(userID, pin)) {
+                    if (pin.equals("0")) {
+                        walletView.displayMessage("Login canceled.");
+                        return;  // Exit the login process
+                    }
+
+                    if (pin.matches("\\d{4}") && userRepository.authenticatePIN(userID, pin)) {
                         walletView.displayMessage("Login successful!\n");
                         ACTIVE_USER = userRepository.getUser(userID);
                         mainMenu();
@@ -65,11 +78,25 @@ public class WalletViewModel {
         String userID;
         String pin;
         do {
+            walletView.displayMessage("\nEnter 0 to cancel registration.");
             userID = walletView.getUserInput("Enter User ID: ");
+
+            // Check if the user entered "0" to cancel the registration
+            if (userID.equals("0")) {
+                walletView.displayMessage("Registration canceled.");
+                return;  // Exit the login process
+            }
         } while (userID == null || !userID.matches("\\d{6}"));
 
         do {
+            walletView.displayMessage("\nEnter 0 to cancel registration.");
             pin = walletView.getUserInput("Enter User Pin: ");
+
+            // Check if the user entered "0" to cancel the registration
+            if (userID.equals("0")) {
+                walletView.displayMessage("Registration canceled.");
+                return;  // Exit the login process
+            }
         } while (pin == null || !pin.matches("\\d{4}"));
 
         String name = walletView.getUserInput("Enter your name: ");
